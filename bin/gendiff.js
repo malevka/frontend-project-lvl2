d6ec5abeb3563eba053e675dbdb015ddc093133a
malevka <formaleva@gmail.com>
2021-03-19 10:43:14 +0300

    feature: add option -f and arguments to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,13 @@ import { Command } from "commander/esm.mjs";
 
 const program = new Command();
 
+program.arguments("<filepath1> <filepath2>");
 program.description("Compares two configuration files and shows a difference.");
 program.version("1.0.0", "-V, --version", "output the version number");
 
-program.option("-h, --help", "output usage information");
+program
+  .option("-h, --help", "output usage information")
+  .option("-f, --format [type]", "output format");
 
 program.parse();
 const options = program.opts();
