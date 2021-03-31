65488d6575e3cc795d7047a4eb1bd3911205306a
malevka <formaleva@gmail.com>
2021-03-31 11:44:48 +0300

    test: add tests for  json and yaml parsers

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,28 @@ import { fileURLToPath } from "url";
 import path from "path";
 import genDiff from "../index.js";
 import readData from "../src/read-data.js";
+import parse from "../src/parsers.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) =>
   path.join(__dirname, "..", "__fixtures__", filename);
+const expectedDiffResult =
+  "{\n" +
+  "  - follow: false\n" +
+  "    host: hexlet.io\n" +
+  "  - proxy: 123.234.53.22\n" +
+  "  - timeout: 50\n" +
+  "  + timeout: 20\n" +
+  "  + verbose: true" +
+  "\n}";
+
+const expectedSourceParsedData = {
+  host: "hexlet.io",
+  timeout: 50,
+  proxy: "123.234.53.22",
+  follow: false,
+};
 
 test("read file content", () => {
   const sourcePath = getFixturePath("test1.json");
@@ -20,13 +37,23 @@ test("return error because of wrong file path", () => {
   expect(() => readData(sourcePath)).toThrow();
 });
 
-test("compare plain json files", () => {
+test("parse json file", () => {
   const sourcePath = getFixturePath("test1.json");
-  const targetPath = getFixturePath("test2.json");
-  const result =
-    "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
+  expect(parse(sourcePath, readData(sourcePath))).toEqual(
+    expectedSourceParsedData
+  );
+});
 
-  expect(genDiff(sourcePath, targetPath)).toMatch(result);
+test("parse yaml file", () => {
+  const sourcePath = getFixturePath("test1.yml");
+  expect(parse(sourcePath, readData(sourcePath))).toEqual(
+    expectedSourceParsedData
+  );
+});
+
+test("parse json file with incorrect format", () => {
+  const sourcePath = getFixturePath("incorrect.json");
+  expect(() => parse(sourcePath, readData(sourcePath))).toThrow();
 });
 
 test("compare files with wrong paths", () => {
@@ -36,4 +63,18 @@ test("compare files with wrong paths", () => {
   const expectedResult = `'${path.resolve(sourcePath)}' does not exist`;
   genDiff(sourcePath, targetPath);
   expect(consoleSpy).toHaveBeenCalledWith(expectedResult);
+});
+
+test("compare plain json files", () => {
+  const sourcePath = getFixturePath("test1.json");
+  const targetPath = getFixturePath("test2.json");
+
+  expect(genDiff(sourcePath, targetPath)).toMatch(expectedDiffResult);
+});
+
+test("compare plain yaml files", () => {
+  const sourcePath = getFixturePath("test1.yml");
+  const targetPath = getFixturePath("test2.yml");
+
+  expect(genDiff(sourcePath, targetPath)).toMatch(expectedDiffResult);
 });
