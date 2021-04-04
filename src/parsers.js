2da42d9c8837458b3e2199d8372aa8e1d8f612e7
malevka <formaleva@gmail.com>
2021-04-04 23:56:35 +0300

    refactor: change parsers select

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,16 @@
 import path from 'path';
 import yaml from 'js-yaml';
 
+const getParser = (format) => {
+  if (format === '.yml') return yaml.safeLoad;
+  if (format === '.json') return JSON.parse;
+  throw new Error(`unsupported extension ${format}`);
+};
 export default (filepath, data) => {
   const format = path.extname(filepath);
-  let { parse } = JSON;
-  if (format === '.yml') parse = yaml.safeLoad;
+  const parser = getParser(format);
   try {
-    return parse(data);
+    return parser(data);
   } catch (e) {
     throw new Error(`'${filepath}' has incorrect format`);
   }
