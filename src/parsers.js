96b645c2a560d45241566f2b105cfce362eb5311
malevka <formaleva@gmail.com>
2021-04-10 15:12:15 +0300

    refactor: select parser by format, not extension

--- src/parsers.js
+++ src/parsers.js
@@ -1,17 +1,16 @@
-import path from 'path';
 import yaml from 'js-yaml';
 
 const getParser = (format) => {
-  if (format === '.yml') return yaml.safeLoad;
-  if (format === '.json') return JSON.parse;
-  throw new Error(`unsupported extension ${format}`);
+  const mapping = {
+    yml: yaml.safeLoad,
+    json: JSON.parse,
+  };
+  return mapping[format];
 };
-export default (filepath, data) => {
-  const format = path.extname(filepath);
+export default (format, data) => {
   const parser = getParser(format);
-  try {
-    return parser(data);
-  } catch (e) {
-    throw new Error(`'${filepath}' has incorrect format`);
+  if (parser === undefined) {
+    throw new Error(`unsupported format ${format}`);
   }
+  return parser(data);
 };
