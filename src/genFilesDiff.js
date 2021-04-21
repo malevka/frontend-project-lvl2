ef4fcb9b2c47ea7dab441a734bab37a50a944775
malevka <formaleva@gmail.com>
2021-04-21 12:27:04 +0300

    refactor:  use helper getExt for determining format

--- src/genFilesDiff.js
+++ src/genFilesDiff.js
@@ -4,13 +4,12 @@ import buildDiffTree from './buildDiffTree.js';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 
+const getExt = (filepath) => path.extname(filepath).slice(1);
 export default (sourceFilepath, targetFilepath, formatName = 'stylish') => {
   const sourceData = readFileSync(path.resolve(sourceFilepath), 'utf-8');
   const targetData = readFileSync(path.resolve(targetFilepath), 'utf-8');
-  const sourceFormat = path.extname(sourceFilepath).slice(1);
-  const targetFormat = path.extname(targetFilepath).slice(1);
-  const sourceParsedData = parse(sourceData, sourceFormat);
-  const targetParsedData = parse(targetData, targetFormat);
+  const sourceParsedData = parse(sourceData, getExt(sourceFilepath));
+  const targetParsedData = parse(targetData, getExt(targetFilepath));
   const diffTree = buildDiffTree(sourceParsedData, targetParsedData);
   return format(diffTree, formatName);
 };
