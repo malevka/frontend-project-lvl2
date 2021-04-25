e4b7b2e1de941a8025940f5ef94075005970655f
malevka <formaleva@gmail.com>
2021-04-25 19:59:18 +0300

    refactor: refactor formatter plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,40 +1,29 @@
 import _ from 'lodash';
 import TYPES from '../types.js';
-import isNode from '../nodes.js';
 
-const formatValue = (value) => {
+const stringifyValue = (value) => {
   if (_.isObject(value)) return '[complex value]';
   if (typeof value === 'string') return `'${value}'`;
   return value;
 };
-const buildLog = (path, type, value, newValue) => {
-  const stringifiedPath = path.join('.');
-  switch (type) {
-    case TYPES.ADDED:
-      return `Property '${stringifiedPath}' was added with value: ${formatValue(value)}`;
-    case TYPES.CHANGED:
-      return `Property '${stringifiedPath}' was updated. From ${formatValue(
-        value,
-      )} to ${formatValue(newValue)}`;
-    default:
-      return `Property '${stringifiedPath}' was removed`;
-  }
+const formatAsPlain = (diff, parentKey = '') => {
+  const result = diff.map(({
+    key, value, type, child, newValue,
+  }) => {
+    const path = parentKey !== '' ? `${parentKey}.${key}` : key;
+    switch (type) {
+      case TYPES.ADDED:
+        return `Property '${path}' was added with value: ${stringifyValue(value)}`;
+      case TYPES.CHANGED:
+        return `Property '${path}' was updated. From ${stringifyValue(value)} to ${stringifyValue(newValue)}`;
+      case TYPES.REMOVED:
+        return `Property '${path}' was removed`;
+      case TYPES.PARENT:
+        return formatAsPlain(child, path);
+      default:
+        return '';
+    }
+  });
+  return _.compact(result).join('\n');
 };
-export default (diff) => {
-  const iter = (data, path) => {
-    if (!isNode(data)) return [];
-    const result = data.flatMap(({
-      key, value, type, child, newValue,
-    }) => {
-      if (type !== TYPES.PARENT && type !== TYPES.UNCHANGED) {
-        return buildLog([...path, key], type, value, newValue);
-      }
-      if (!_.isUndefined(child)) {
-        return iter(child, [...path, key]);
-      }
-      return [];
-    });
-    return result.join('\n');
-  };
-  return iter(diff, []);
-};
+export default formatAsPlain;
